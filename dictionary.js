252c49915cfd59d61fac3a6750ae09d1c7e6a7f1
jhjang <jhjang@useed.co.kr>
2018-11-06 18:24:38 +0900

    dictionary 추가

---
+++ dictionary.js
@@ -0,0 +1,25 @@
+
+var dictionary = {
+  "noun":[],
+  "verb":[],
+  "adjective":[],
+  "adverb":[],
+  "pronoun":[],
+  "preposition":[],
+  "conjunction":[
+    ["and", "also", "besides", "futhermore", "likewise", "moreover"],
+    ["but", "yet", "however", "nevertheless", "still", "instead", "otherwise", "rather"],
+    ["so", "accordingly", "consequently", "hence", "meanwhile", "then", "therefore", "thus"],
+    ["though", "although", "even though", "while"],
+    ["if", "only if", "unless", "until", "provided that", "assuming that", "even if", "in case that"],
+    ["after", "as long as", "as soon as", "before", "by the time", "now that", "once", "since", "till", "until", "when", "whenever", "while"],
+    ["that", "what", "whatever", "which", "whichever"],
+    ["who", "whoever", "whom", "whomever", "whose"],
+    ["than", "rather than", "whether", "as much as", "whereas"],
+    ["because", "since", "so that", "why"],
+    ["how", "as though", "as if"],
+    ["where", "wherever"]
+  ],
+  "determiner":[],
+  "exclamation":[]
+}
